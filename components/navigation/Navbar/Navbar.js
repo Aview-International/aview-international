ab53409fea5bdbbaa600430cc89e3b8bc326813b
Andrew Qiao <andrewqiao2004@gmail.com>
2022-05-27 15:20:15 -0400

    Reset navbar code

--- components/navigation/Navbar/Navbar.js
+++ components/navigation/Navbar/Navbar.js
@@ -5,44 +5,7 @@ import Link from "next/link";
  * Top navbar
  */
 function Navbar() {
-  return (
-    <header>
-      <Link href="/">
-        <Image class="logo" alt="Aview logo" src="img/logo.svg" />
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Creators</a>
-          </li>
-          <li>
-            <a href="#">Translators</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-        </ul>
-      </nav>
-      <a href="#generate-aview">Contact Us</a>
-      <MenuButton />
-      <div id="close-btn">
-        <Image src="img/close-icon.svg" alt="Close icon" />
-      </div>
-    </header>
-  );
+  return <div></div>;
 }
 
 export default Navbar;
-
-function MenuButton() {
-  return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
-}
