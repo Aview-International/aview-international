eb015de31df134db6c9ec1b0411663ea1d6f2eac
Andrew Qiao <andrewqiao2004@gmail.com>
2022-05-29 14:25:32 -0400

    Form logic

--- components/form/Input/Input.js
+++ components/form/Input/Input.js
@@ -1,6 +1,10 @@
-import Border from "../../UI/Border/Border";
+import Image from 'next/image';
 
-import styles from "./Input.module.css";
+import Border from '../../UI/Border/Border';
+
+import correctIcon from '../../../public/img/icons/correct.svg';
+import incorrectIcon from '../../../public/img/icons/incorrect.svg';
+import styles from './Input.module.css';
 
 /**
  * Text input for forms
@@ -8,16 +12,39 @@ import styles from "./Input.module.css";
  * @prop type: Type of text input
  * @prop placeholder: Placeholder text for input
  * @prop bgColor: Background color of input (light or dark)
+ * @prop changeHandler: Function run when input value is changed
  */
-function Input({ type, placeholder, bgColor }) {
+function Input({
+  type,
+  placeholder,
+  bgColor,
+  changeHandler,
+  value,
+  valid,
+  hasSubmitted,
+}) {
   return (
-    <Border borderRadius="3px" gradientReverse={true}>
-      <input
-        type={type}
-        placeholder={placeholder}
-        className={`${styles.input} ${styles[bgColor]}`}
-      />
-    </Border>
+    <div className={styles['input-container']}>
+      <Border borderRadius="3px" gradientReverse={true}>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={`${styles.input} ${styles[bgColor]}`}
+          onChange={changeHandler}
+          value={value}
+        />
+      </Border>
+      {!valid && hasSubmitted && (
+        <div className={styles.icon}>
+          <Image src={incorrectIcon} alt="Incorrect input icon" />
+        </div>
+      )}
+      {valid && (
+        <div className={styles.icon}>
+          <Image src={correctIcon} alt="Correct input icon" />
+        </div>
+      )}
+    </div>
   );
 }
 
---
+++ utils/submit-form.js
@@ -0,0 +1,18 @@
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
+
+export function submitForm(name, data) {
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode({
+      'form-name': name,
+      ...data,
+    }),
+  })
+    .then(() => true)
+    .catch((error) => alert(error));
+}
