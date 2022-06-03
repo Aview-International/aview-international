363132697b441c5444a2fb83772c2e7e393f70ac
Andrew Qiao <andrewqiao2004@gmail.com>
2022-06-02 22:19:00 -0400

    Revert "Merge pull request #5 from Aview-International/TJ"
    This reverts commit 1f270f0cd0ec9fd93efc78b2fb2c65089dbd3c69, reversing
    changes made to cb55b58df4ec5270f55aa846c6f925085b9042d3.

--- components/sections/about/Landing/Landing.js
+++ components/sections/about/Landing/Landing.js
@@ -1,32 +1,7 @@
-import Image from "next/image";
-import TextWithImage from "../../../layout/TextWithImage/TextWithImage";
-import Button from "../../../../components/UI/Button/Button";
-import aboutlandingGraphic from "../../../../public/img/graphics/about-landing.png";
 import styles from "./Landing.module.css";
 
 function Landing() {
-  return (
-    <section className="section m-horizontal">
-      <TextWithImage>
-          <div>
-          <h2 className={`section-title ${styles["section-title"]}`}>
-            Our Mission
-          </h2>
-          <p className={`section-description ${styles["section-description"]}`}>
-            To help build content creator brands globally through translated 
-            subtitles and voiceovers; a client, a language, and
-            <span className="gradient-text"> Aview </span> at a time.
-          </p>
-            <Button type="primary" isRoute={false} link="#generate-aview">
-              Contact Us
-            </Button>
-        </div>
-        <div className={styles["about-landing-graphic"]}>
-          <Image src={aboutlandingGraphic} alt="about landing graphic" />
-        </div>
-      </TextWithImage>
-    </section>
-  );
+  return <></>;
 }
 
 export default Landing;
