15ac3993cd9ebde8df3cb9687f13e3d9f3417d27
Andrew Qiao <andrewqiao2004@gmail.com>
2022-06-03 12:31:14 -0400

    Add blobs

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,15 @@
-import Navbar from "../../components/navigation/Navbar/Navbar";
-import Landing from "../../components/sections/about/Landing/Landing";
-import AboutAview from "../../components/sections/about/AboutAview/AboutAview";
-import AtAview from "../../components/sections/about/AtAview/AtAview";
-import CoreValues from "../../components/sections/about/CoreValues/CoreValues";
-import Growth from "../../components/sections/about/Growth/Growth";
-import Milestones from "../../components/sections/about/Milestones/Milestones";
-import MeetTeam from "../../components/sections/about/MeetTeam/MeetTeam";
-import Name from "../../components/sections/about/Name/Name";
-import JoinTeam from "../../components/sections/about/JoinTeam/JoinTeam";
-import Footer from "../../components/navigation/Footer/Footer";
+import Navbar from '../../components/navigation/Navbar/Navbar';
+import Landing from '../../components/sections/about/Landing/Landing';
+import AboutAview from '../../components/sections/about/AboutAview/AboutAview';
+import AtAview from '../../components/sections/about/AtAview/AtAview';
+import CoreValues from '../../components/sections/about/CoreValues/CoreValues';
+import Growth from '../../components/sections/about/Growth/Growth';
+import Milestones from '../../components/sections/about/Milestones/Milestones';
+import MeetTeam from '../../components/sections/about/MeetTeam/MeetTeam';
+import Name from '../../components/sections/about/Name/Name';
+import JoinTeam from '../../components/sections/about/JoinTeam/JoinTeam';
+import Footer from '../../components/navigation/Footer/Footer';
+import Blobs from '../../components/UI/Blobs/Blobs';
 
 function About() {
   return (
@@ -24,6 +25,7 @@ function About() {
       <Name />
       <JoinTeam />
       <Footer />
+      <Blobs />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,31 @@
-import Navbar from "../components/navigation/Navbar/Navbar";
-import Landing from "../components/sections/home/Landing/Landing";
-import WeTranslate from "../components/sections/home/WeTranslate/WeTranslate";
-import LeadInTranslations from "../components/sections/home/LeadInTranslations/LeadInTranslations";
-import BreadAndButter from "../components/sections/home/BreadAndButter/BreadAndButter";
-import ContentCreators from "../components/sections/home/ContentCreators/ContentCreators";
-import Videos from "../components/sections/home/Videos/Videos";
-import Generate from "../components/sections/home/Generate/Generate";
-import FAQs from "../components/sections/home/FAQs/FAQs";
-import GoGlobal from "../components/sections/home/GoGlobal/GoGlobal";
-import Footer from "../components/navigation/Footer/Footer";
+import Navbar from '../components/navigation/Navbar/Navbar';
+import Landing from '../components/sections/home/Landing/Landing';
+import WeTranslate from '../components/sections/home/WeTranslate/WeTranslate';
+import LeadInTranslations from '../components/sections/home/LeadInTranslations/LeadInTranslations';
+import BreadAndButter from '../components/sections/home/BreadAndButter/BreadAndButter';
+import ContentCreators from '../components/sections/home/ContentCreators/ContentCreators';
+import Videos from '../components/sections/home/Videos/Videos';
+import Generate from '../components/sections/home/Generate/Generate';
+import FAQs from '../components/sections/home/FAQs/FAQs';
+import GoGlobal from '../components/sections/home/GoGlobal/GoGlobal';
+import Footer from '../components/navigation/Footer/Footer';
+import Blobs from '../components/UI/Blobs/Blobs';
 
 function Home() {
   return (
     <>
       <Navbar />
-      <main>
-        <Landing />
-        <WeTranslate />
-        <LeadInTranslations />
-        <BreadAndButter />
-        <ContentCreators />
-        <Videos />
-        <Generate />
-        <FAQs />
-        <GoGlobal />
-      </main>
+      <Landing />
+      <WeTranslate />
+      <LeadInTranslations />
+      <BreadAndButter />
+      <ContentCreators />
+      <Videos />
+      <Generate />
+      <FAQs />
+      <GoGlobal />
       <Footer />
+      <Blobs />
     </>
   );
 }
