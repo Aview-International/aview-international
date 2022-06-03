13f7afc80af7aa91f74330eb4f035f8107c20623
Andrew Qiao <andrewqiao2004@gmail.com>
2022-06-03 11:52:10 -0400

    Complete benefits of translations section

---
+++ components/layout/Row2/Row2.js
@@ -0,0 +1,7 @@
+import styles from './Row2.module.css';
+
+function Row2({ children }) {
+  return <div className={styles.row}>{children}</div>;
+}
+
+export default Row2;
--- components/sections/creators/TranslationBenefits/TranslationBenefits.js
+++ components/sections/creators/TranslationBenefits/TranslationBenefits.js
@@ -1,7 +1,61 @@
+import Row2 from '../../../layout/Row2/Row2';
+import GraphicCard from '../../../UI/GraphicCard/GraphicCard';
+
+import visibilityGraphic from '../../../../public/img/graphics/visibility.png';
+import growthGraphic from '../../../../public/img/graphics/growth.png';
+import friendlyGraphic from '../../../../public/img/graphics/user-friendly.png';
+import expandGraphic from '../../../../public/img/graphics/expand.png';
 import styles from './TranslationBenefits.module.css';
 
+const CARD_CONTENT = [
+  {
+    title: 'Visibility',
+    description:
+      "Translated metadata will help increase your visibility on Youtube's algorithm.",
+    graphic: visibilityGraphic,
+    alt: 'visibility graphic',
+  },
+  {
+    title: 'Growth',
+    description:
+      'Increase the growth of your channel by tapping into new communities.',
+    graphic: growthGraphic,
+    alt: 'growth graphic',
+  },
+  {
+    title: 'User-friendly',
+    description: 'Make your videos more accessible to different groups. ',
+    graphic: friendlyGraphic,
+    alt: 'user-friendly graphic',
+  },
+  {
+    title: 'Expand',
+    description: 'Grow an international audience and increase engagement. ',
+    graphic: expandGraphic,
+    alt: 'expand graphic',
+  },
+];
+
 function TranslationBenefits() {
-  return <></>;
+  return (
+    <section className="section m-horizontal">
+      <h2 className={`gradient-text section-title ${styles['section-title']}`}>
+        Benefits of Translations
+      </h2>
+      <Row2>
+        {CARD_CONTENT.map((content, i) => (
+          <GraphicCard
+            key={`card-${i}`}
+            title={content.title}
+            description={content.description}
+            graphic={content.graphic}
+            alt={content.alt}
+            horizontal={true}
+          />
+        ))}
+      </Row2>
+    </section>
+  );
 }
 
 export default TranslationBenefits;
