7fc4a3717826de1da8fec0317927a1c9f8013597
Andrew Qiao <andrewqiao2004@gmail.com>
2022-06-08 06:22:48 -0400

    Revert "Merge pull request #21 from Aview-International/taehun"
    This reverts commit f69c64e86696cf1232c16e6895b85d9638ed326c, reversing
    changes made to 44c146f8de27cb8631c7ae20bbc9b6d81a7b889d.

--- components/sections/about/AtAview/AtAview.js
+++ components/sections/about/AtAview/AtAview.js
@@ -1,20 +1,7 @@
-import GradientFullWidth from '../../../layout/GradientFullWidth/GradientFullWidth';
 import styles from "./AtAview.module.css";
 
 function AtAview() {
-  return (
-    <section className="section">
-      <GradientFullWidth>
-        <h2 className={`section-title ${styles['section-title']}`}>
-          At Aview we
-        </h2>
-        <p className={`section-description ${styles['section-description']}`}>
-        Ensure your content is translated accurately,
-        guaranteeing a positive return.
-        </p>
-      </GradientFullWidth>
-    </section>
-  );
+  return <></>;
 }
 
 export default AtAview;
