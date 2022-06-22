1e80c50fb83cb2b185e1fb3d82f6bc911c95182e
Andrew Qiao <andrewqiao2004@gmail.com>
2022-06-21 22:47:21 -0400

    Fix bug on translator page landing

--- components/sections/translators/FavoriteInfluencer/FavoriteInfluencer.js
+++ components/sections/translators/FavoriteInfluencer/FavoriteInfluencer.js
@@ -4,25 +4,24 @@ import styles from './FavoriteInfluencer.module.css';
 import landingImage from '../../../../public/img/graphics/translator-landing.png';
 
 function FavoriteInfluencer() {
-    <section className={`section m-horizontal ${styles.section}`}>
-      <div>
-        <h1 className={`section-title ${styles.title}`}>
-          <span className="gradient-text">
-            Translate Your Favorite Influencer Videos!
-          </span>
-        </h1>
-        <p className={`section-description ${styles.description}`}>
-          Apply now to join our team of translators, dubbers and editors.
-        </p>
-        <Button type="primary" link="/">
-          Contact Us
-        </Button>
-      </div>
-      <div className={styles['landing-image']}>
-        <Image src={landingImage} alt="landing graphic" />
-      </div>
-    </section>
-  );
+  <section className={`section m-horizontal ${styles.section}`}>
+    <div>
+      <h1 className={`section-title ${styles.title}`}>
+        <span className="gradient-text">
+          Translate Your Favorite Influencer Videos!
+        </span>
+      </h1>
+      <p className={`section-description ${styles.description}`}>
+        Apply now to join our team of translators, dubbers and editors.
+      </p>
+      <Button type="primary" link="/">
+        Contact Us
+      </Button>
+    </div>
+    <div className={styles['landing-image']}>
+      <Image src={landingImage} alt="landing graphic" />
+    </div>
+  </section>;
 }
 
 export default FavoriteInfluencer;
