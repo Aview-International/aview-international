3f1554f70d3cb8be894e8e928b82c04b7b56fe9b
mac <mac@Ace-2.local>
2022-06-26 19:23:00 +0100

    Process section on Translators page

--- components/UI/DottedBorder/DottedBorder.js
+++ components/UI/DottedBorder/DottedBorder.js
@@ -1,7 +1,25 @@
 import styles from './DottedBorder.module.css';
 
-const DottedBorder = ({ children }) => {
-  return <div className={styles.DottedBorder}>{children}</div>;
+/**
+ * DottedBorder Component
+ *
+ * @prop children - Children to be rendered inside the border
+ * @prop borderRadius: Radius of the border, defaults to 15px
+ *
+ * @author Victor Ogunjobi
+ *
+ */
+const DottedBorder = ({ children, borderRadius }) => {
+  return (
+    <div
+      className={`${styles.DottedBorder}`}
+      style={{
+        borderRadius: borderRadius,
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default DottedBorder;
