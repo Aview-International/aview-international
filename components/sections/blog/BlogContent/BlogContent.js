4b7dfcb0d54de1281bb2a36f2d45aeca5f570592
Andrew Qiao <andrewqiao2004@gmail.com>
2022-07-04 09:43:00 -0400

    Start working on blog page

---
+++ components/sections/blog/BlogContent/BlogContent.js
@@ -0,0 +1,9 @@
+import styles from './BlogContent.module.css';
+
+function BlogContent() {
+  <section className={`section m-horizontal`}>
+    <h2 className={`section-title ${styles.title}`}></h2>
+  </section>;
+}
+
+export default BlogContent;
---
+++ pages/blog/index.js
@@ -0,0 +1,15 @@
+import Navbar from '../../components/navigation/Navbar/Navbar';
+import Footer from '../../components/navigation/Footer/Footer';
+import Blobs from '../../components/UI/Blobs/Blobs';
+
+function Blog() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+      <Blobs />
+    </>
+  );
+}
+
+export default Blog;
