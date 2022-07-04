9fd12eb17ecad822b1972321227f014cd42be101
Taehun Jang <taehunjang93@gmail.com>
2022-07-04 10:00:18 -0500

    Update whatisbill
    Update: whatisbill

--- components/sections/bill-c96/WhatIs/WhatIs.js
+++ components/sections/bill-c96/WhatIs/WhatIs.js
@@ -1,7 +1,18 @@
 import styles from './WhatIs.module.css';
 
 function WhatIs() {
-  return <></>;
+  return (
+    <section className={`section m-horizontal ${styles.section}`}>
+      <p className={styles.intro}>
+        <span className="gradient-text">
+          What is Bill C-96
+        </span>
+      </p>
+      <h2 className={`section-title ${styles.title}`}>
+      Bill C-96 reinforces the obligation of businesses offering goods and services to consumers to respect their right to be served in French.
+      </h2>
+    </section>
+  );
 }
 
 export default WhatIs;
