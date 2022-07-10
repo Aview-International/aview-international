c3a2b13925ac5dcafc69c93aa8362aeb2b10ff57
Taehun Jang <taehunjang93@gmail.com>
2022-07-10 11:51:16 -0500

    update 404
    update: 404page

---
+++ pages/404/index.js
@@ -0,0 +1,32 @@
+import styles from './404.module.css';
+import Image from 'next/image';
+import graphic from '../../public/img/graphics/404Image.png';
+import Button from '../../components/UI/Button/Button';
+
+function error404() {
+  return (
+    <section className={`section m-horizontal ${styles.section}`}>
+      <div className={styles.graphic}>
+      <Image src={graphic} />
+      </div>
+      <div className={styles.text}>
+      <h1 className={`section-title ${styles.title}`}>
+          <span className="gradient-text">Page Not Found</span></h1>
+          <p className={`section-description ${styles['section-description']}`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <p className={`section-description ${styles['section-description']}`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <p className={`section-description ${styles['section-description']}`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+      </div>
+      <Button type="primary" isOnClick={true} onClick="history.back()">
+        Go Back
+      </Button>
+    </section>
+  );
+}
+
+export default error404;
