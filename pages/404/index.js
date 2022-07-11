16c591a16e34ab1feb9f71381ebbbe687858a194
Victor <Vicopem@gmail.com>
2022-07-11 14:08:03 +0100

    Error 404 minor adjustments

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,32 +1,22 @@
 import styles from './404.module.css';
 import Image from 'next/image';
-import graphic from '../../public/img/graphics/404Image.png';
+import Graphic from '../../public/img/graphics/404Image.png';
 import Button from '../../components/UI/Button/Button';
 
-function error404() {
+function Error404() {
   return (
     <section className={`section m-horizontal ${styles.section}`}>
       <div className={styles.graphic}>
-      <Image src={graphic} />
+        <Image src={Graphic} alt="Page not found" layout="responsive" />
       </div>
-      <div className={styles.text}>
       <h1 className={`section-title ${styles.title}`}>
-          <span className="gradient-text">Page Not Found</span></h1>
-          <p className={`section-description ${styles['section-description']}`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-          <p className={`section-description ${styles['section-description']}`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-          <p className={`section-description ${styles['section-description']}`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-      </div>
-      <Button type="primary" isRoute={true} link="/">
-        Go Back
+        <span className="gradient-text">Page Not Found</span>
+      </h1>
+      <Button type="primary" link="/">
+        Go Home
       </Button>
     </section>
   );
 }
 
-export default error404;
+export default Error404;
